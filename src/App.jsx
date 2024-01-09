3741950df123c21e9513775110a108911415a92f
Shweta Arora <sa4563095@gmail.com>
2024-01-09 13:31:14 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello Universe</h1>
     </>
   );
 }
