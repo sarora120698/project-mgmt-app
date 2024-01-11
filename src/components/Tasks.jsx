868c51586872b8be1fbcd3e569fc32e25c9771a5
Shweta Arora <sa4563095@gmail.com>
2024-01-11 12:01:46 +0530

    stable version upto lecture 180 - adding tasks to the project

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,13 @@
+import NewTask from "./NewTask";
+export default function Tasks() {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      <NewTask />
+      <p className="text-stone-800 my-4">
+        The project does not have any tasks yet.
+      </p>
+      <ul className="p-4 mt-8 rounded-md bg-stone-100"></ul>
+    </section>
+  );
+}
